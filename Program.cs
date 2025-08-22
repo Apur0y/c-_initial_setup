12be49e216d56b9faba9908e7dd259de000fb987
Apu Roy <apuroy2785@gmail.com>
2025-08-22 09:58:21 +0600

    added some api

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
- 
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+}
 app.UseHttpsRedirection();
+
+//REST API 
+app.MapGet("/", () => {
+    return "Server is running";
+});
+app.MapGet("/user", () =>
+{
+    return "Hello From get";
+});
+
+app.MapPost("/user", () =>
+{
+    return "Hi from POST";
+});
+
+app.MapPut("/user", () =>
+{
+    return "Hi from PUT";
+});
+
+app.MapDelete("/user", () =>
+{
+    return "Hi from DELETE";
+});
 
 app.Run();
 
