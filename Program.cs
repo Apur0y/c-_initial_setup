24afc39a5de25fff2827b99878ccc35bb8f33688
Apu Roy <apuroy2785@gmail.com>
2025-08-22 10:55:18 +0600

    Set up the records

--- Program.cs
+++ Program.cs
@@ -9,16 +9,18 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+    app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
 
 //REST API 
-app.MapGet("/", () => {
-    return "Server is running";
-});
+app.MapGet("/", () => "Server is running");
+
 app.MapGet("/user", () =>
 {
-    return "Hello From get";
+    var response = new { message = "This is  a json Object", succces=true};
+
+    return Results.Ok(response);
 });
 
 app.MapPost("/user", () =>
@@ -36,5 +38,25 @@ app.MapDelete("/user", () =>
     return "Hi from DELETE";
 });
 
+var products = new List<Products>() {
+    new Products("Apple", 299),
+    new Products("Samsung", 399),
+};
+
+app.MapGet("/products", () =>
+{
+    return Results.Ok(products);
+});
+
 app.Run();
 
+
+public record Products(string Name, decimal Price);
+
+public record Category
+{
+     public Guid CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    
+}
