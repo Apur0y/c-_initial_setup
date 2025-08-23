3aecc498076997c2533f8916f6cefb95c3b2fe6c
Apu Roy <apuroy2785@gmail.com>
2025-08-23 10:30:24 +0600

    CRUD methos

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 
+List<Category> categories = new List<Category>(); 
+
 //REST API 
 app.MapGet("/", () => "Server is running");
 
@@ -48,6 +50,45 @@ app.MapGet("/products", () =>
     return Results.Ok(products);
 });
 
+app.MapPost("/api/categories", () =>
+{
+    var category = new Category
+    {
+        CategoryId = Guid.Parse("3f2504e0-4f89-11d3-9a0c-0305e82c3301"
+),
+        Name = "Samsung",
+        Description = "This the best phone",
+        CreatedAt = DateTime.Now,
+    };
+    categories.Add(category);
+    return Results.Created($"/api/categories/{category.CategoryId}", category);
+}); 
+
+app.MapDelete("/api/categories", () =>
+{
+    var foundCategory = categories.FirstOrDefault(Category => Category.CategoryId == Guid.Parse("3f2504e0-4f89-11d3-9a0c-0305e82c3301"
+));
+    if (foundCategory is null)
+    {
+        return Results.NotFound("Category with this id no not");
+    }
+    categories.Remove(foundCategory);
+    return Results.NoContent();
+}); 
+
+app.MapPut("/api/categories", () =>
+{
+    var foundCategory = categories.FirstOrDefault(Category => Category.CategoryId == Guid.Parse("3f2504e0-4f89-11d3-9a0c-0305e82c3301"
+));
+    if (foundCategory is null)
+    {
+        return Results.NotFound("Category with this id no not");
+    }
+         foundCategory.Name = "Updated Name";
+         foundCategory.Description = "Updated Description";
+    return Results.NoContent();
+}); 
+
 app.Run();
 
 
@@ -55,8 +96,9 @@ public record Products(string Name, decimal Price);
 
 public record Category
 {
-     public Guid CategoryId { get; set; }
+    public Guid CategoryId { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
     
 }
