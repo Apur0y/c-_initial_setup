9bda0eb4d6624e47e918965e59a7d0090a399399
Apu Roy <apuroy2785@gmail.com>
2025-08-29 11:47:30 +0600

    added the MVC

---
+++ Controlers/CategoryControler.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using first_net.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace first_net.Controlers
+{
+    [ApiController]
+    [Route("api/categories/")]
+    public class CategoryControler : ControllerBase
+    {
+        private static List<Category> categories = new List<Category>();
+
+        //GET: /api/categoris=>Read categories
+        [HttpGet]
+        public IActionResult GetCategories([FromQuery] string searchValue = "")
+        {
+            Console.WriteLine($"{searchValue}");
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                var res = categories.Where(c => c.Name.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+                return Ok(res);
+            }
+
+            return Ok(categories);
+        }
+
+
+
+        //POST: /api/categoris=>Read categories
+        [HttpPost]
+        public IActionResult CreateCategories([FromBody] Category categoryData)
+        {
+
+            if (string.IsNullOrEmpty(categoryData.Name))
+            {
+                return BadRequest("Catefory name required and not be empty");
+            }
+            // Console.WriteLine($"This is my bojdy: {categoryData}");
+            var category = new Category
+            {
+                CategoryId = Guid.NewGuid(),
+                Name = categoryData.Name,
+                Description = categoryData.Description,
+                CreatedAt = DateTime.Now,
+            };
+            categories.Add(category);
+            return Created($"/api/categories/{category.CategoryId}", category);
+
+        }
+
+
+
+        //DELETE: /api/categoris=>Read categories
+        [HttpDelete("{categoryId:guid}")]
+        public IActionResult DeleteCategories(Guid categoryId)
+        {
+            var foundCategory = categories.FirstOrDefault(Category => Category.CategoryId == categoryId);
+            Console.WriteLine(foundCategory);
+            if (foundCategory is null)
+            {
+                return NotFound("Category with this id no not");
+            }
+            categories.Remove(foundCategory);
+            return NoContent();
+
+        }
+
+
+        //PUT: /api/categoris=>Read categories
+        [HttpPut("{categoryId:guid}")]
+        public IActionResult UpdateCategories(Guid categoryId, [FromBody] Category categoryData)
+        {
+             var foundCategory = categories.FirstOrDefault(Category => Category.CategoryId == categoryId);
+    Console.WriteLine(foundCategory);
+    if (foundCategory is null)
+    {
+        return NotFound("Category with this id not found");
+    }
+    if (categoryData is null)
+    {
+        return BadRequest("CategoryData is missing!");
+    }
+    if (!string.IsNullOrEmpty(categoryData.Name))
+    {
+        if (categoryData.Name.Length >= 2)
+        {
+
+            foundCategory.Name = categoryData.Name;
+        }
+        else
+        {
+           return BadRequest("Name must be at least two charecter");
+        }
+    }
+    if (!string.IsNullOrEmpty(categoryData.Description))
+    {
+        foundCategory.Description = categoryData.Description;
+    }
+    return Ok(foundCategory);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Add services to the controler
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
@@ -15,97 +17,15 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 
-List<Category> categories = new List<Category>(); 
-
-var products = new List<Products>() {
-    new Products("Apple", 299),
-    new Products("Samsung", 399),
-};
-
-app.MapGet("/api/categories", ([FromQuery] string searchValue="") =>
+app.MapGet("/", () =>
 {
-    Console.WriteLine($"{searchValue}");
-    if (!string.IsNullOrEmpty(searchValue))
-    {
-       var res= categories.Where(c => c.Name.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
-            return Results.Ok(res);
-    }
-    
-    return Results.Ok(categories);
+    Console.WriteLine("Server is running");
 });
 
-app.MapPost("/api/categories", ([FromBody] Category categoryData) =>
-{
-    if (string.IsNullOrEmpty(categoryData.Name))
-    {
-        return Results.BadRequest("Catefory name required and not be empty");
-    }
-    // Console.WriteLine($"This is my bojdy: {categoryData}");
-    var category = new Category
-    {
-        CategoryId = Guid.NewGuid(),
-        Name =categoryData.Name,
-        Description=categoryData.Description,
-        CreatedAt = DateTime.Now,
-    };
-    categories.Add(category);
-    return Results.Created($"/api/categories/{category.CategoryId}", category);
-
-}); 
-
-app.MapDelete("/api/categories/{categoryId:guid}", (Guid categoryId) =>
-{
-    var foundCategory = categories.FirstOrDefault(Category => Category.CategoryId == categoryId);
-    Console.WriteLine(foundCategory);
-    if (foundCategory is null)
-    {
-        return Results.NotFound("Category with this id no not");
-    }
-    categories.Remove(foundCategory);
-    return Results.NoContent();
-}); 
-
-app.MapPut("/api/categories/{categoryId}", (Guid categoryId, [FromBody] Category categoryData) =>
-{
-    var foundCategory = categories.FirstOrDefault(Category => Category.CategoryId == categoryId);
-    Console.WriteLine(foundCategory);
-    if (foundCategory is null)
-    {
-        return Results.NotFound("Category with this id not found");
-    }
-    if (categoryData is null)
-    {
-        return Results.BadRequest("CategoryData is missing!");
-    }
-    if (!string.IsNullOrEmpty(categoryData.Name))
-    {
-        if (categoryData.Name.Length > 2)
-        {
-
-            foundCategory.Name = categoryData.Name;
-        }
-        else
-        {
-           return Results.BadRequest("Name must be at least two charecter");
-        }
-    }
-    if (!string.IsNullOrEmpty(categoryData.Description))
-    {
-        foundCategory.Description = categoryData.Description;
-    }
-    return Results.Ok(foundCategory);
-}); 
-
+app.MapControllers();
+                                    
 app.Run();
 
 
 public record Products(string Name, decimal Price);
 
-public record Category
-{
-    public Guid CategoryId { get; set; }
-    public string Name { get; set; }
-    public string? Description { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; }
-    
-}
